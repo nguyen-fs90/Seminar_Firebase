5eca7330174a0c0660a0ead8025e119feb21d972
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-05-18 20:58:45 +0900

    demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,15 +2,16 @@ import { Switch, Route } from "react-router-dom";
 
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import NoMatch from './components/NoMatch';
+import DashBoard from './pages/DashBoard';
+import NotFound from './components/NotFound';
 
 export default function App() {
   return(
     <Switch>
-      <Route exact path="/"><h1>Home</h1></Route>
+      <Route exact path="/"><DashBoard /></Route>
       <Route exact path="/login"><Login /></Route>
       <Route exact path="/signup"><SignUp /></Route>
-      <Route path="*"><NoMatch /></Route>
+      <Route path="*"><NotFound /></Route>
     </Switch>
   );
 }
